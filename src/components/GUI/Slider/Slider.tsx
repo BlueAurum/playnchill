3ecab70c1c77f58c95ebdc4ec2c98d7c9c3fbfef
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2023-06-08 09:36:53 +0300

    slider development

--- src/components/GUI/Slider/Slider.tsx
+++ src/components/GUI/Slider/Slider.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Carousel, Row, Col } from "antd";
 import slide_one from "../../../../public/assets/slider_1.png";
 import slide_two from "../../../../public/assets/slider_2.jpg";
@@ -6,14 +7,32 @@ import slide_three from "../../../../public/assets/slider_3.jpeg";
 import slide_right from "../../../../public/assets/slider_2.png";
 import slide_left from "../../../../public/assets/slider_3.png";
 
+import arraw_right from "./assets/icons/arraw_right.svg";
+import arraw_left from "./assets/icons/arraw_left.svg";
+
+import style from "./Slider.module.css";
+
 export const Slider: React.FC = () => {
+  const carouselRef: React.Ref<any> = React.createRef();
+
+  const prevSlide = () => {
+    carouselRef.current.prev();
+  };
+
+  const nextSlide = () => {
+    carouselRef.current.next();
+  };
+
   return (
     <Row justify={"center"}>
       <Col>
         <img style={{ opacity: 0.4, marginRight: 10 }} src={slide_right} alt="" />
+        <div>
+          <img onClick={prevSlide} className={style.arraw__slider__right} src={arraw_left} alt="" />
+        </div>
       </Col>
       <Col span={12}>
-        <Carousel>
+        <Carousel ref={carouselRef}>
           <div>
             <img style={{ height: "679px" }} src={slide_one} alt="" />
           </div>
@@ -27,6 +46,9 @@ export const Slider: React.FC = () => {
       </Col>{" "}
       <Col>
         <img style={{ opacity: 0.4, marginLeft: 10 }} src={slide_left} alt="" />
+        <div className={style.arraw__slider__left}>
+          <img onClick={nextSlide} src={arraw_right} alt="" />
+        </div>
       </Col>
     </Row>
   );
